87c1eda4f58990faf76db33945ddb492d293d4f0
faassen <faassen@startifact.com>
2014-03-04 16:47:13 +0100

    Update to use newer version of mapply.

--- hireg/mapply.pyx
+++ hireg/mapply.pyx
@@ -1,11 +1,7 @@
-# cython: profile=True
 import inspect
 
-VARARGS = 4
-KWARGS = 8
 
-
-def mapply(object func, *args, **kw):
+def mapply(func, *args, **kw):
     """Apply keyword arguments to function only if it defines them.
 
     So this works without error as ``b`` is ignored::
@@ -22,114 +18,140 @@ def mapply(object func, *args, **kw):
     return cymapply(func, args, kw)
 
 cdef object cymapply(object func, tuple args, dict kw):
-    cdef tuple argnames
-    cdef int varargs
-    cdef int kwargs
     cdef dict new_kw
     cdef CacheEntry info
     info = arginfo(func)
-    if info.kwargs:
+    if info.keywords:
         return func(*args, **kw)
-    new_kw = {name: kw[name] for name in info.argnames if name in kw}
+    # XXX we don't support nested arguments
+    new_kw = {name: kw[name] for name in info.args if name in kw}
     return func(*args, **new_kw)
+
 
 cdef dict _arginfo_cache
 
 _arginfo_cache = {}
 
 
+
+cdef class CacheEntry:
+    cdef tuple args
+    cdef int varargs
+    cdef int keywords
+
+    def __init__(self, args, varargs, keywords):
+        self.args = args
+        self.varargs = varargs
+        self.keywords = keywords
+
+
+cdef CacheEntry arginfo(object callable):
+    """Get information about the arguments of a callable.
+
+    Returns a :class:`inspect.ArgSpec` object as for
+    :func:`inspect.getargspec`.
+
+    :func:`inspect.getargspec` returns information about the arguments
+    of a function. arginfo also works for classes and instances with a
+    __call__ defined. Unlike getargspec, arginfo treats bound methods
+    like functions, so that the self argument is not reported.
+
+    arginfo caches previous calls (except for instances with a
+    __call__), making calling it repeatedly cheap.
+
+    This was originally inspired by the pytest.core varnames() function,
+    but has been completely rewritten to handle class constructors,
+    also show other getarginfo() information, and for readability.
+    """
+    try:
+        return _arginfo_cache[callable]
+    except KeyError:
+        # Try to get __call__ function from the cache.
+        try:
+            return _arginfo_cache[callable.__call__]
+        except (AttributeError, KeyError):
+            pass
+    func, cache_key, remove_self = get_callable_info(callable)
+    if func is None:
+        return inspect.ArgSpec([], None, None, None)
+    argspec = inspect.getargspec(func)
+    if remove_self:
+        args = argspec.args[1:]
+        argspec = inspect.ArgSpec(args, argspec.varargs, argspec.keywords,
+                                  argspec.defaults)
+    result = _arginfo_cache[cache_key] = CacheEntry(
+        tuple(argspec.args),
+        argspec.varargs is not None,
+        argspec.keywords is not None)
+    return result
+
+
+def is_cached(callable):
+    if callable in _arginfo_cache:
+        return True
+    return callable.__call__ in _arginfo_cache
+
+
+def get_callable_info(callable):
+    """Get information about a callable.
+
+    Returns a tuple of:
+
+    * actual function/method that can be inspected with inspect.getargspec.
+
+    * cache key to use to cache results.
+
+    * whether to remove self or not.
+
+    Note that in Python 3, __init__ is not a method, but we still
+    want to remove self from it.
+
+    If not inspectable (None, None, False) is returned.
+    """
+    if inspect.isfunction(callable):
+        return callable, callable, False
+    if inspect.ismethod(callable):
+        return callable, callable, True
+    if inspect.isclass(callable):
+        return get_class_init(callable), callable, True
+    try:
+        callable = getattr(callable, '__call__')
+        return callable, callable, True
+    except AttributeError:
+        return None, None, False
+
+
+def fake_empty_init():
+    pass
+
+
 class Dummy(object):
     pass
 
 
-class InitDummy(object):
-    def __init__(self):
-        pass
-
-
 WRAPPER_DESCRIPTOR = Dummy.__init__
 
-cdef class CacheEntry:
-    cdef tuple argnames
-    cdef int varargs
-    cdef int kwargs
 
-    def __init__(self, argnames, varargs, kwargs):
-        self.argnames = argnames
-        self.varargs = varargs
-        self.kwargs = kwargs
-
-cdef CacheEntry arginfo(object func):
-    """Get arg names and kw arg flag for given function or method or
-    constructor.
-
-    Taken from pytest.core, varnames. Adjusted to get argument names
-    for class constructors too and record keyword arguments.
-    """
+def get_class_init(class_):
     try:
-        return _arginfo_cache[func]
-    except KeyError:
-        pass
-    origfunc = func
-    if (not inspect.isfunction(func) and
-        not inspect.ismethod(func) and
-        not inspect.isclass(func)):
-        func = getattr(func, '__call__', func)
-    if inspect.isclass(func):
-        try:
-            func = func.__init__
-            # new-style class without __init__, not pypy
-            if func is WRAPPER_DESCRIPTOR:
-                return bare_arginfo(origfunc)
-        except AttributeError:
-            # classic class without __init__
-            return bare_arginfo(origfunc)
-    ismethod = inspect.ismethod(func)
-    rawcode = getrawcode(func)
-    # new-style class without __init__, pypy
-    try:
-        if rawcode.co_name == 'descr__init__':
-            return bare_arginfo(origfunc)
+        func = class_.__init__
     except AttributeError:
-        # not a function
-        return bare_arginfo(origfunc)
+        # Python 2 classic class without __init__.
+        return fake_empty_init
+    # If this is a new-style class and there is no __init__
+    # defined, in CPython (but not PyPy) this is a WRAPPER_DESCRIPTOR.
+    if func is WRAPPER_DESCRIPTOR:
+        return fake_empty_init
+    # A PyPy class without __init__ needs to be handled specially,
+    # as the default __init__ in this case falsely reports varargs
+    # and keywords.
+    if is_pypy_default_init(func):
+        return fake_empty_init
+    return func
+
+
+def is_pypy_default_init(func):
     try:
-        argnames = rawcode.co_varnames[ismethod:rawcode.co_argcount]
+        return func.func_code.co_name == 'descr__init__'
     except AttributeError:
-        argnames = ()
-    try:
-        varargs = bool(rawcode.co_flags & VARARGS)
-    except AttributeError:
-        varargs = False
-    try:
-        kwargs = bool(rawcode.co_flags & KWARGS)
-    except AttributeError:
-        kwargs = False
-    cdef CacheEntry result
-    result =_arginfo_cache[origfunc] = CacheEntry(argnames, varargs, kwargs)
-    return result
-
-
-def bare_arginfo(origfunc):
-    _arginfo_cache[origfunc] = [], False, False
-    return [], False, False
-
-
-def getrawcode(obj, trycall=True):
-    """Return code object for given function.
-
-    Taken from py._code.code
-    """
-    try:
-        return obj.__code__
-    except AttributeError:
-        obj = getattr(obj, 'im_func', obj)
-        obj = getattr(obj, 'func_code', obj)
-        obj = getattr(obj, 'f_code', obj)
-        obj = getattr(obj, '__code__', obj)
-        if trycall and not hasattr(obj, 'co_firstlineno'):
-            if hasattr(obj, '__call__') and not inspect.isclass(obj):
-                x = getrawcode(obj.__call__, trycall=False)
-                if hasattr(x, 'co_firstlineno'):
-                    return x
-        return obj
+        return False
